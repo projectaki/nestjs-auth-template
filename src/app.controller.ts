16b703990e07c3b780c9159a478db13beb617341
Akos <akosegypro@gmail.com>
2022-02-12 19:58:13 +0100

    🪛 Init environment variables + default route ascii art

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Res } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Res() res) {
+    res.send(
+      `<html><body><pre>
+
+                                          ,--,    
+                                 ,----,,---.'|    
+         .---.    ,---,.       .'   .'||   | :    
+         /. ./|  ,'  .' |    .'   .'   ;:   : |    
+    .--'.  ' ;,---.'   |  ,---, '    .'|   ' :    
+    /__./ \\ : ||   |   .'  |   :     ./ ;   ; '    
+ .--'.  '   \\' .:   :  |-,  ;   | .'  /  '   | |__  
+/___/ \\ |    ' ':   |  ;/|  '---' /  ;   |   | :.'| 
+ ;   \\  \\;      :|   :   .'    /  ;  /    '   :    ; 
+   \\   ;  '      ||   |  |-,   ;  /  /--,  |   |  ./  
+.   \\    .\\  ;'   :  ;/|  /  /  / .'|  ;   : ;    
+  \\   \\   ' \\ ||   |    \\./__;       :  |   ,/     
+  :   '  |--" |   :   .'|   :     .'   '---'      
+   \\   \\ ;    |   | ,'  ;   |  .'                 
+    '---"     '----'    '---'                     
+                                                                                                          
+      
+      
+      </pre></body></html>`
+    );
   }
 }
